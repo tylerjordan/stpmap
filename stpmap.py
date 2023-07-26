9ddf1f7a91066bdf400c44c898706218feb60fe7
tjordan <tyler@ezrahjade.com>
2023-07-26 11:26:59 -0400

    testing tables

--- stpmap.py
+++ stpmap.py
@@ -167,8 +167,7 @@ def oper_commands(my_ips):
                         for vlan_id in stpbridge:
                             if vlan_id.vlan_id == '111':
                                 print("{}: {}: {}: {}".format(vlan_id.vlan_id, vlan_id.root_bridge_mac,
-                                                          vlan_id.local_bridge_priority, vlan_id.root_port))
-                        print(stpbridge.items())
+                                                          vlan_id.local_bridge_mac, vlan_id.root_port))
                         print("\n**************\n")
                         lldpneigh = LLDPNeighborTable(jdev)
                         lldpneigh.get()
